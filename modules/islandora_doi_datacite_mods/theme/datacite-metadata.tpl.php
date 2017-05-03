549aa04915183b077aca7e637aa52a50eac3dc5b
Mark Jordan <mjordan@sfu.ca>
2017-05-03 03:47:19 +0000

    Additional work.

--- modules/islandora_doi_datacite_mods/theme/datacite-metadata.tpl.php
+++ modules/islandora_doi_datacite_mods/theme/datacite-metadata.tpl.php
@@ -4,11 +4,9 @@
  * @file
  * Theme template for the DataCite metadata record.
  */
-<?xml version = "1.0" encoding = "UTF-8"?>
+?>
 <resource xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://datacite.org/schema/kernel-4" xsi:schemaLocation="http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd">
-  <!-- Required -->
   <identifier identifierType="DOI">10.5072/D3P26Q35R-Test</identifier>
-  <!-- Required -->
   <creators>
     <?php foreach($creators as $creator) : ?>
     <creator>
@@ -16,32 +14,33 @@
     </creator>
       <?php endforeach; ?>
   </creators>
-  <!-- Required -->
   <titles>
     <?php foreach($titles as $title): ?>
     <title><?php print $title; ?></title>
     <?php endforeach; ?>
   </titles>
-  <!-- Required -->
+  <?php foreach($publishers as $publisher): ?>
   <publisher><?php print $publisher; ?></publisher>
-  <!-- Required -->
+   <?php endforeach; ?>
+  <?php foreach($publication_years as $publication_year): ?>
   <publicationYear><?php print $publication_year; ?></publicationYear>
-  <?php if (count($subjects)): ?>
+  <?php endforeach; ?>
+  <?php if (isset($subjects) && count($subjects)): ?>
   <subjects>
     <?php foreach($subjects as $subject): ?>
     <subject><?php print $subject; ?></subject>
     <?php endforeach; ?>
   </subjects>
   <?php endif; ?>
-  <?php if ($language): ?>
+  <?php if (isset($languages) && count($languages)): ?>
+    <?php foreach($languages as $language): ?>
     <language><?php print $language; ?></language>
+    <?php endforeach; ?>
   <?php endif; ?>
-  <!-- Required -->
+  <?php foreach($resource_types as $resource_type): ?>
   <resourceType resourceTypeGeneral="<?php print $resource_type; ?>"><?php print $resource_type; ?></resourceType>
-  <?php if ($version): ?>
-  <version><?php print $version; ?></version>
-  <?php endif; ?>
-  <?php if (count($descriptions)): ?>
+  <?php endforeach; ?>
+  <?php if (isset($descriptions) && count($descriptions)): ?>
   <descriptions>
     <?php foreach($descriptions as $description): ?>
     <description descriptionType="Abstract">
