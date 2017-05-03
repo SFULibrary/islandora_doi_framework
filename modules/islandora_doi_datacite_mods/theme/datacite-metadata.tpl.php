f524cbe8e4bf1f1cec6fc5769b9e9932b4b758f6
Mark Jordan <mjordan@sfu.ca>
2017-05-03 05:12:49 +0000

    Theme template cleanup; better drupal_set_messages; some additional cleanup.

--- modules/islandora_doi_datacite_mods/theme/datacite-metadata.tpl.php
+++ modules/islandora_doi_datacite_mods/theme/datacite-metadata.tpl.php
@@ -6,30 +6,30 @@
  */
 ?>
 <resource xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://datacite.org/schema/kernel-4" xsi:schemaLocation="http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd">
-  <identifier identifierType="DOI">10.5072/D3P26Q35R-Test</identifier>
+  <identifier identifierType="DOI"><?php print $doi; ?></identifier>
   <creators>
-    <?php foreach($creators as $creator) : ?>
+  <?php foreach($creators as $creator) : ?>
     <creator>
       <creatorName><?php print $creator; ?></creatorName>
     </creator>
-      <?php endforeach; ?>
+  <?php endforeach; ?>
   </creators>
   <titles>
-    <?php foreach($titles as $title): ?>
+  <?php foreach($titles as $title): ?>
     <title><?php print $title; ?></title>
-    <?php endforeach; ?>
+  <?php endforeach; ?>
   </titles>
   <?php foreach($publishers as $publisher): ?>
   <publisher><?php print $publisher; ?></publisher>
-   <?php endforeach; ?>
+  <?php endforeach; ?>
   <?php foreach($publication_years as $publication_year): ?>
   <publicationYear><?php print $publication_year; ?></publicationYear>
   <?php endforeach; ?>
   <?php if (isset($subjects) && count($subjects)): ?>
   <subjects>
-    <?php foreach($subjects as $subject): ?>
+  <?php foreach($subjects as $subject): ?>
     <subject><?php print $subject; ?></subject>
-    <?php endforeach; ?>
+  <?php endforeach; ?>
   </subjects>
   <?php endif; ?>
   <?php if (isset($languages) && count($languages)): ?>
